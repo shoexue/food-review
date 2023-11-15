63211f4fac4a7fdd8abc58d927dce7b51332ef84
Jonathan Lamontagne Kratz <77954172+epicgamer17@users.noreply.github.com>
2023-11-15 18:22:20 -0500

    Image downloading for posts

---
+++ src/pages/admin/create-post/index.tsx
@@ -0,0 +1,157 @@
+import downloadjs from 'downloadjs';
+import html2canvas from 'html2canvas';
+import { cn, formatDate, getImage } from "@/lib/utils"
+import StarMeter from '@/components/StarMeter';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from '@/components/ui/card';
+import Image from 'next/image'
+
+import Spinner from '@/components/Spinner';
+import { observer } from 'mobx-react-lite';
+import { store } from '@/lib/types';
+import { useState } from 'react';
+import ReviewModal from '@/components/ReviewModal';
+import { StarIcon as StarSolidIcon } from '@heroicons/react/24/solid';
+import { StarIcon as StarOutlineIcon } from '@heroicons/react/24/outline';
+
+const CreatePost = observer(() => {
+    const handleCaptureClick = async () => {
+        const slide1Element =
+            document.querySelector<HTMLElement>('.slide1');
+        if (!slide1Element) return;
+
+        const canvas1 = await html2canvas(slide1Element);
+        const dataURL1 = canvas1.toDataURL('image/png');
+        downloadjs(dataURL1, 'slide1.png', 'image/png');
+
+
+
+        const slide2Element =
+            document.querySelector<HTMLElement>('.slide2');
+        if (!slide2Element) return;
+
+        const canvas2 = await html2canvas(slide2Element);
+        const dataURL2 = canvas2.toDataURL('image/png');
+        downloadjs(dataURL2, 'slide2.png', 'image/png');
+
+
+        // const slide3Element =
+        //     document.querySelector<HTMLElement>('.slide3');
+        // if (!slide3Element) return;
+
+        // const canvas3 = await html2canvas(slide3Element);
+        // const dataURL3 = canvas3.toDataURL('image/png');
+        // downloadjs(dataURL3, 'slide3.png', 'image/png');
+    };
+
+    const { items, itemsInitialized, diningHalls, settings } = store;
+
+    const [reviewModalOpen, setReviewModalOpen] = useState(false);
+    const [reviewItemId, setReviewItemId] = useState('');
+
+    return (
+        <div className='flex flex-col items-center gap-y-4 mx-12 md:mx-48'>
+            <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center'>
+                Post Generator!
+            </h1>
+            <Button onClick={handleCaptureClick}>
+                Download Post Image
+            </Button>
+            {!itemsInitialized ? (
+                <Spinner />
+            ) : (
+                <>
+                    <div className='grid w-full grid-cols-1 gap-4'>
+                        {items
+                            .map((i) => (
+                                <div className='flex flex-row' key={i.id}>
+                                    <Card className='w-full aspect-square slide1'>
+                                        <div className='' key={i.id}>
+                                            <CardContent className='flex items-center justify-center h-96'>
+                                                <div className='relative w-full h-full'>
+                                                    <Image src={getImage(i.imageUrl)} fill alt='' />
+                                                </div>
+                                            </CardContent>
+                                        </div>
+                                        <div className='absolute'>
+                                            <CardHeader className='flex flex-row justify-between bg-transparent'>
+                                                <div>
+                                                    <CardTitle className=''>
+                                                        {i.name}
+                                                    </CardTitle>
+                                                    <CardDescription>
+                                                        {`${i.reviews.reviews.length} total reviews`}
+                                                    </CardDescription>
+                                                </div>
+                                                <div className='flex flex-row items-center'>
+                                                </div>
+                                            </CardHeader>
+                                            <CardContent className='mx-6 pb-2'>
+                                                <StarMeter stars={i.rating} />
+                                                <p className='inline'>
+                                                    {Math.round((i.rating + Number.EPSILON) * 10) / 10}/10
+                                                </p>
+                                            </CardContent>
+                                            <CardFooter className='flex flex-col justify-between items-start h-32'>
+                                                <div className='flex flex-wrap items-center overflow-y-hidden'>
+                                                    {/* <Badge variant="default">
+                                                    {diningHalls.halls.get(i.diningHall)?.name}
+                                                </Badge> */}
+                                                    {/* {i.tags.map((tagId) => {
+                                                    return (
+                                                        <Badge
+                                                            variant="outline"
+                                                            key={tagId}
+                                                        >
+                                                            {tags.tags.get(tagId)?.value}
+                                                        </Badge>
+                                                    );
+                                                })} */}
+                                                </div>
+                                            </CardFooter>
+                                        </div>
+                                    </Card>
+
+
+                                    <Card className='w-full aspect-square slide2'>
+                                        <div className='flex flex-col h-full m-10'>
+                                            <CardHeader>
+                                                <div>
+                                                    <CardTitle>{i.reviews.getItems()[0].title}</CardTitle>
+                                                    <CardDescription>
+                                                        {formatDate(new Date(i.reviews.getItems()[0].createdAt))}
+                                                    </CardDescription>
+                                                </div>
+                                            </CardHeader>
+                                            <CardContent className='m-auto'>
+                                                <p className='ml-6 my-2'>{i.reviews.getItems()[0].comment}</p>
+                                                <div className='flex flex-row items-center justify-center w-full mb-10'>
+                                                    {[...Array(i.reviews.getItems()[0].rating)].map((s, i) => {
+                                                        return <StarSolidIcon key={i} className='w-8 h-8' />;
+                                                    })}
+                                                    {[...Array(10 - i.reviews.getItems()[0].rating)].map((s, i) => {
+                                                        return <StarOutlineIcon key={i} className='w-8 h-8' />;
+                                                    })}
+                                                    {/* <p className='align-middle'>{i.reviews.getItems()[0].rating}/10</p> */}
+                                                </div>
+                                            </CardContent>
+                                            <CardFooter>
+
+                                            </CardFooter>
+                                        </div>
+                                    </Card>
+                                </div>
+                            ))}
+                    </div>
+                </>
+            )}
+            <ReviewModal
+                open={reviewModalOpen}
+                onClose={() => setReviewModalOpen(false)}
+                itemId={reviewItemId}
+            />
+        </div>
+    );
+});
+export default CreatePost
+
